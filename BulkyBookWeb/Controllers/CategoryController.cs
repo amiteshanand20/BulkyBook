98d94bad0c3b65c2835d156a4da72f9a4ed2dc50
amitesh <amiteshanand20@gmail.com>
2023-05-28 23:50:06 +0530

    Category View and controller added

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController( ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> ObjCategoryList = _db.categories;
+            return View(ObjCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
