871397c85f86b6f72ddccb9eda9184c7771334f7
amitesh <amiteshanand20@gmail.com>
2024-09-04 12:35:07 +0530

    Add to cart

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using BulkyBook.Models.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting.Internal;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BulkyBookWeb.Controllers
 {
@@ -23,16 +25,30 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties : "Category");
             return View(productList);
         }
+
         public IActionResult Details(int productId)
         {
-            ShoppingCart cart = new ShoppingCart()
+            ShoppingCart cart = new()
             {
                 Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
-                Count=1,
-                ProductId=productId
+                Count = 1,
+                ProductId = productId
             };
-           
             return View(cart);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            _unitOfWork.ShoppingCart.Add(shoppingCart);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
         }
         public IActionResult Privacy()
         {
